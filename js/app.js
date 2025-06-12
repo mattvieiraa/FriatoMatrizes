62e7440f6d1ca01462dfd09fab118f4e29458703
Mateus Vieira <mateus_vieira1@hotmail.com>
2025-06-12 19:58:18 -0300

    Versão 1.2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,69 @@
+// Seleciona as linagens
+const linhagem = document.getElementById('linhagemGranja');
+
+function mostrarLinagem() {
+    let idade = document.getElementById('idade').value;
+    let racaoRecomendada = document.getElementById('resultadoIdade');
+    if (linhagem.value === 'linhagemCobb'){
+        if (idade == 0 || idade == "") {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Por favor, insira uma idade válida.</p>`;
+        } else if (idade == 1) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração recomendada:<br> <strong>MATRIZ PRE INICIAL – COBB ROSS HBB</strong></p>`;
+        } else if (idade >= 2 && idade <= 4) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração recomendada:<br> <strong>MATRIZ INICIAL – COBB</strong></p>`;
+        } else if (idade >= 5 && idade <= 18) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração recomendada:<br> <strong>MATRIZ CRESCIMENTO - COBB</strong></p>`;
+        } else if (idade >= 19 && idade <= 24) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ PRÉ POSTURA – COBB ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;
+        } else if (idade >= 25 && idade <= 27) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 1 – COBB</strong> <br>Ração machos recomendada:<br> <strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;
+        } else if (idade >= 28 && idade <= 38) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 1 – COBB</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO - COBB</strong></p>`;
+        } else if (idade >= 39 && idade <= 50) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 2 – COBB</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO - COBB</strong></p>`;
+        } else {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 3 – COBB</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO - COBB</strong></p>`;
+        }
+    } else if (linhagem.value === 'linhagemRoss'){
+        if (idade == 0 || idade == "") {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Por favor, insira uma idade válida.</p>`;
+        } else if (idade == 1) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração recomendada:<br> <strong>MATRIZ PRE INICIAL – COBB ROSS HBB</strong></p>`;
+        } else if (idade >= 2 && idade <= 6) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração recomendada:<br> <strong>MATRIZ INICIAL – ROSS HBB</strong></p>`;
+        } else if (idade >= 7 && idade <= 16) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;
+        } else if (idade >= 17 && idade <= 23) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ PRÉ POSTURA – COBB ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;            
+        } else if (idade >= 24 && idade <= 27) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 1 – ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;
+        } else if (idade >= 28 && idade <= 34) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 1 – ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO – ROSS</strong></p>`;
+        } else if (idade >= 35 && idade <= 49) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 2 – ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO – ROSS</strong></p>`;
+        } else  {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 3 – ROSS</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO – ROSS</strong></p>`;
+        }
+    } else {
+        if (idade == 0 || idade == "") {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Por favor, insira uma idade válida.</p>`;
+        } else if (idade == 1) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração recomendada:<br> <strong>MATRIZ PRE INICIAL – COBB ROSS HBB</strong></p>`;
+        } else if (idade >= 2 && idade <= 6) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração recomendada:<br> <strong>MATRIZ INICIAL – ROSS HBB</strong></p>`;
+        } else if (idade >= 7 && idade <= 16) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;
+        } else if (idade >= 17 && idade <= 23) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ PRÉ POSTURA – COBB ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;            
+        } else if (idade >= 24 && idade <= 28) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 1 – ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>MATRIZ CRESCIMENTO – COBB</strong></p>`;
+        } else if (idade >= 29 && idade <= 44) {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 1 – ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO – COBB</strong></p>`;
+        } else {
+            racaoRecomendada.innerHTML = `<p id="resultadoIdade">Ração fêmeas recomendada:<br> <strong>MATRIZ POSTURA 2 – ROSS HBB</strong> <br>Ração machos recomendada:<br> <strong>RAÇÃO DE MACHO – COBB</strong></p>`;
+        }
+}
+
 // Seleciona os botões
 const btnCobb = document.getElementById('btnCobb');
 const btnRoss = document.getElementById('btnRoss');
